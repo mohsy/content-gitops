1b0ae6a30aa5ad078333538052d00e9a145e7700
mohsy <52632506+mohsy@users.noreply.github.com>
2021-01-21 20:41:05 +0000

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello GITWORLD!"
+    return "Hello GITWORLD! FEATURE-BRANCH-2"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
